1dc3403431a8f13253027da26e4c47754bfa9d85
Frey Hargarten <jeff.hargarten@startribune.com>
2016-10-26 10:32:34 -0500

    updates

--- elex_summary/elexbars_desktop.js
+++ elex_summary/elexbars_desktop.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 
 page.open('http://static.startribune.com/news/elections/elex-graphics-banner.html', function() {
   // being the actual size of the headless browser
-  page.viewportSize = { width: 1250, height: 90 };
+  page.viewportSize = { width: 970, height: 90 };
 
   var clipRect = page.evaluate(function(){
     return document.querySelector('#snapme').getBoundingClientRect();
