e2b60ddba80c8b880ddaffec37855e899a52e71b
Frey Hargarten <jeff.hargarten@startribune.com>
2017-02-01 16:20:32 -0600

    campfi nonsense

--- campaign_finance/data/fuckingmergeshit.py
+++ campaign_finance/data/fuckingmergeshit.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-a = pd.read_csv("CandDiscFilings.csv")
-b = pd.read_csv("cand.csv")
+a = pd.read_csv("candidates_master.csv")
+b = pd.read_csv("CandDiscFilings.csv")
 b = b.dropna(axis=1)
 merged = a.merge(b, on='CandRegistrationNumber')
-merged.to_csv("candidates_master.csv", index=False)
+merged.to_csv("candidates_master2.csv", index=False)
